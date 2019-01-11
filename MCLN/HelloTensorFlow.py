4d4e5a655dbb6d78b2b6b217c76e4908274d7eac
rmaxod76 <rmaxod76@gmail.com>
2019-01-12 00:41:55 +0900

    2019.01.12 폴더 정리함

---
+++ MCLN/HelloTensorFlow.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+tf.__version__
+
+hello = tf.constant("Hello, TensorFlow!")
+
+sess = tf.Session()
+
+print(sess.run(hello))
+
+node1 = tf.constant(3.0, tf.float32)
+node2 = tf.constant(4.0)
+node3 = tf.add(node1, node2)
+
+print("node1:", node1, "node2:", node2)
+print("node3:", node3)
